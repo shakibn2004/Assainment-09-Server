17ac1888bbf645528ee07aa98c8ca7bd9f779cb0
shakibn2004 <shakibn2004@gmail.com>
2026-05-20 01:46:02 +0600

    all most all routes completed

---
+++ index.js
@@ -0,0 +1,92 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true
+}));
+
+app.use(express.json());
+
+
+// database and collection create and send data
+let allpets;
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db("assainment-09-server");
+        allpets = db.collection('allpets');
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+run();
+
+app.get('/', async (req, res) => {
+    try {
+        const cursor = allpets.find();
+        const result = await cursor.toArray();
+        console.log(result);
+
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Failed to fetch users',
+            error: error.message,
+        });
+    }
+});
+
+
+    // Get Single User By ID
+    app.get('/public/all-pets/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const query = {
+          _id: new ObjectId(id),
+        };
+
+        const result = await allpets.findOne(query);
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: 'Failed to fetch user',
+          error: error.message,
+        });
+      }
+    });
+
+
+
+
+app.post('/dashboard/add-pet', async (req, res) => {
+    const result = await allpets.insertOne(req.body);
+    res.send(result);
+    console.log(result);
+});
+
+app.listen(8000, () => {
+    console.log('Server running');
+});
